9b4cf07278eca6caddd1d34bf3b2de8d66eedd6f
Myron Apostolakis <myron.apostolakis@sunlight.io>
2021-12-12 14:35:28 +0200

    routes for crud operations in effects

---
+++ protectedRoutes/effects/getEffects.js
@@ -0,0 +1,46 @@
+var mysql = require("mysql");
+var DB = require("helpers/db");
+
+/* GET effects. */
+const getEffects = async (req, res, next) => {
+  console.log(
+    "DB",
+    DB,
+    "host:",
+    DB.host,
+    " username ",
+    DB.username,
+    " pwd:",
+    DB.password
+  );
+
+  const connection = mysql.createConnection({
+    host: DB.host,
+    user: DB.username,
+    port: DB.port,
+    password: DB.password,
+    database: DB.databaseName,
+  });
+
+  connection.connect();
+
+  const sql = `SELECT Possible_failures.possible_failure_id,
+       Possible_failures.failure_description,
+       Possible_failures.system_part_id,
+       System_parts.procedure_step,
+       System_parts.variable
+    FROM Possible_failures
+    LEFT JOIN System_parts ON Possible_failures.system_part_id = System_parts.system_part_id`;
+
+  await connection.query(sql, function (err, result) {
+    if (err) throw err;
+
+    console.log("Got effects data", result);
+
+    res.json({ results: result });
+  });
+
+  connection.end();
+};
+
+module.exports = getEffects;
